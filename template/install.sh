f9ebfbaca4f35bb2ca3addad05333c77bd65f200
Ben Lau <xbenlau@gmail.com>
2019-06-29 17:49:05 +0800

    Fix installation problem of using install.sh

--- template/install.sh
+++ template/install.sh
@@ -6,4 +6,4 @@ INSTALL_PATH="$HOME/.config/QtProject/qtcreator/templates/wizards/$NAME"
 SOURCE_PATH=$(dirname $0)
 
 mkdir -p "$INSTALL_PATH"
-cp -va "$SOURCE_PATH/$NAME"/* "$INSTALL_PATH"
+cp -va "$SOURCE_PATH/$NAME"/. "$INSTALL_PATH"
